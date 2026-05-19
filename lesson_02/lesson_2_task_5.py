d0ec5160501842fd539ee8bf394d71951560e1b5
Sapronov-Nikolay <avindialit@list.ru>
2026-05-20 00:04:56 +0300

    УПР 5: Убрано лишнее, оставлен один вариант решения

--- lesson_02/lesson_2_task_5.py
+++ lesson_02/lesson_2_task_5.py
@@ -1,12 +1,4 @@
-# Для автоматической перестройки в выводе в терменале кирилицы. Чтобы не → �������, а нормально было.
-# Или, если лень, просто запускай в терминале chcp 65001 перед выполнением. 1 раз на сеанс
-import sys, io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
-
-# ========== ДАЛЕЕ КОД УПРАЖНЕНИЯ ==========
-
 # ЗАДАНИЕ: Месяц - сезон
-# ВАРИАНТ 1
 def month_to_season(month):
   if month in (12, 1, 2):
     return "Зима"
@@ -20,24 +12,3 @@ def month_to_season(month):
     return "Введён неверный номер месяца"
   
 print(month_to_season(2))
-
-# ВАРИАНТ 2 С input продолжение варианта без начала функции... она из первоно варианта...
-try:
-  user_input = int(input("Введите номер месяца (1-12): "))
-  season = month_to_season(user_input)
-  print(f"Месяц {user_input} относится к сезону: {season}")
-except ValueError:
-  print("Ошибка! Введите целое число.")
-
-# ВАРИАНТ 3 Через список.
-def month_to_season_list(month):
-  seasons = ["", "Зима", "Зима", "Весна", "Весна", "Весна", "Лето", "Лето", "Лето", "Осень", "Осень", "Осень", "Зима"]
-  if 1 <= month <= 12:
-    return seasons[month]
-  return "Неверный номер месяца"
-try:
-  user_input = int(input("Введите номер месяца (1-12): "))
-  season = month_to_season_list(user_input)
-  print(f"Месяц {user_input} относится ксезону: {season}")
-except ValueError:
-  print("Ошибка! Введите целое число")
