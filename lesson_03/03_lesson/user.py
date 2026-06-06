247c2dc229491a994aa6923f7b2c2d123224290d
Sapronov-Nikolay <avindialit@list.ru>
2026-06-06 11:00:12 +0300

    Выполнили Задание 1. Создание класса

--- lesson_03/03_lesson/user.py
+++ lesson_03/03_lesson/user.py
@@ -1,15 +1,20 @@
-"""
-Создайте файл user.py.
-В файле объявите класс User.
-Объявите в классе конструктор.
-Конструктор должен принимать на вход два параметра:
+# 1. Создайте файл user.py.
+# 2. В файле объявите класс User.
+class User:
+# 3. Объявите в классе конструктор.
+    def __init__(self, first_name, last_name):
+# Конструктор должен принимать на вход два параметра:
+# first_name — имя;
+# last_name — фамилия.
+        self.first_name = first_name
+        self.last_name = last_name
+#
+# 4. Создайте в классе три метода, которые печатают: имя, фамилию, имя и фамилию.
+    def print_first_name(self):
+        print(self.first_name)
 
-first_name — имя;
-last_name — фамилия.
-Помните, что все методы класса, включая конструктор, принимают первым параметром self.
+    def print_last_name(self):
+        print(self.last_name)
 
-Создайте в классе три метода, которые печатают:
-имя,
-фамилию,
-имя и фамилию.
-"""
+    def print_full_name(self):
+        print(self.first_name + ' ' + self.last_name)
